fc43a30f93839fdb07272e38c988c30f6ea98449
Мыльцев Александр Михайлович <myltsev_a@tass.ru>
2022-11-25 11:54:18 +0300

    more cosmetic output changes and clear screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from winreg import *
+import os
 
 from fp.fp import FreeProxy
 
@@ -35,7 +36,7 @@ def changeProxyState():
                 proxy = FreeProxy(elite=1, https=0, country_id='US').get().replace("http://", "")
             except BaseException as e:
                 print(str(e), "Failed to fetch a proxy. Retying...")
-        print("Found proxy:", proxy)
+        print("\r\nFound proxy:", proxy)
 
         try:
             SetValueEx(aKey, "ProxyServer", 0, REG_SZ, proxy)
@@ -45,14 +46,16 @@ def changeProxyState():
         SetValueEx(aKey, "ProxyEnable", 0, REG_DWORD, val)
     except EnvironmentError:
         print("Encountered problems writing into the Registry!")
-    print(f"Proxy is now {'ENABLED!' if val == 1 else 'DISABLED!'}")
+    print(f"\r\n>>Proxy is now {'ENABLED!' if val == 1 else 'DISABLED!'}<<")
     CloseKey(aKey)
     inputVal = input(
-        f"\r\nPress enter to {'ENABLE' if val == 0 else 'DISABLE'} proxy\r\nClose the program, press ctrl+c or type q to exit\r\n")
+        f"\r\nPress ENTER to {'ENABLE' if val == 0 else 'DISABLE'} proxy"
+        "\r\nClose the program, press ctrl+c or type q to exit\r\n")
     if inputVal.lower() == 'q':
         exit()
 
 
 if __name__ == '__main__':
     while True:
+        os.system('cls||clear')
         changeProxyState()
